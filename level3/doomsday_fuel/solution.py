fa4286ca89bcfbec7e385efdbb16b475b84b4b8a
GavinFrazar <gavinfrazar@gmail.com>
2018-04-05 23:11:39 -0700

    test case and setup for solution

---
+++ level3/doomsday_fuel/solution.py
@@ -0,0 +1,19 @@
+def reduceFrac(num, denom):
+    pass
+
+def answer(m):
+    pass
+
+
+# -- test cases --
+m = [
+[0,1,0,0,0,1],
+[4,0,0,3,2,0],
+[0,0,0,0,0,0],
+[0,0,0,0,0,0],
+[0,0,0,0,0,0]
+]
+
+wanted = [0,3,2,9,14]
+print(answer(m))
+print(wanted)
